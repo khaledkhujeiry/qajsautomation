bce0d8376b6f717dbf5ab9400be4574ef36de497
kh_khaled <khaled@yandex.ru>
2024-02-11 17:28:24 +0300

    Fix for any number of digits

--- HomeWork5/2Equality.js
+++ HomeWork5/2Equality.js
@@ -1,24 +1,21 @@
 function checkEqualSums(number) {
-    // Преобразуем число в строку для удобства работы с цифрами
-    const numStr = number.toString();
-  
-    // Проверяем, что строка состоит из шести символов (цифр)
-    if (numStr.length !== 6) {
-      return 'Число должно состоять из шести цифр!';
-    }
-  
-    // Делим строку на две половины (по три цифры в каждой)
-    const firstHalf = numStr.slice(0, 3);
-    const secondHalf = numStr.slice(3);
-  
-    // Суммируем цифры в каждой половине с использованием метода reduce и функции parseInt для преобразования символов в целые числа
-    const sumFirstHalf = firstHalf.split('').reduce((acc, digit) => acc + parseInt(digit), 0);
-    const sumSecondHalf = secondHalf.split('').reduce((acc, digit) => acc + parseInt(digit), 0);
-  
-    // Сравниваем результаты суммирования и возвращаем 'да' или 'нет' в зависимости от результата
-    return sumFirstHalf === sumSecondHalf ? 'Да' : 'Нет';
+  const numStr = number.toString(); // Преобразуем число в строку
+
+  if (numStr.length % 2 !== 0) { // Проверяем, что количество цифр в числе четное
+    return 'Число должно состоять из четного количества цифр!';
   }
-  
-  // Использование функции
-  const number = 123241;
-  console.log(checkEqualSums(number));
+
+  const halfLength = numStr.length / 2; // Вычисляем половину длины строки
+  const firstHalf = numStr.slice(0, halfLength); // Получаем первую половину числа
+  const secondHalf = numStr.slice(halfLength); // Получаем вторую половину числа
+
+  // Суммируем цифры в каждой половине с использованием метода reduce и функции Number для преобразования символов в целые числа
+  const sumFirstHalf = Array.from(firstHalf, Number).reduce((acc, digit) => acc + digit, 0);
+  const sumSecondHalf = Array.from(secondHalf, Number).reduce((acc, digit) => acc + digit, 0);
+
+  // Сравниваем результаты суммирования
+  return sumFirstHalf === sumSecondHalf ? 'Да' : 'Нет';
+}
+
+const number = 12324013;
+console.log(checkEqualSums(number));

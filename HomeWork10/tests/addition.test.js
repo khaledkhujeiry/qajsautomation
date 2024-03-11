df7f9c5e675881cf71f7b5d87dc539ab929f5474
kh_khaled <khaled@yandex.ru>
2024-03-11 22:19:21 +0300

    fixed addition.test.js

--- HomeWork10/tests/addition.test.js
+++ HomeWork10/tests/addition.test.js
@@ -6,13 +6,15 @@ describe("Сложение", () => {
     beforeEach(() => {
         calculator = new Calculator();
     });
+
     it("должно возвращать сумму двух чисел", () => {
         expect(calculator.add(1, 2)).toBe(3);
     });
+
     it("должно возвращать сумму нескольких чисел", () => {
         expect(calculator.add(1, 2, 3, 4, 5)).toBe(15);
     });
-
+    
     it("должно возвращать 0 для пустого массива", () => {
         expect(calculator.add()).toBe(0);
     });
